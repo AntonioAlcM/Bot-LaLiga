612a6b3cf492ef03bf8aef2f854952b39053a3ec
manuelalonsobraojos <manuelalonso136@gmail.com>
2017-01-28 04:56:31 -0800

    Actualizando archivo de Fabric

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,30 @@
 from fabric.api import *
+import os
+
+def info_servidor():
+    run ('uname -s')
 
 def descargar():
     run ('sudo rm -rf proyectoIV')
     run ('sudo git clone https://github.com/manuelalonsobraojos/proyectoIV')
 
 def iniciar():
-    run ('sudo python proyectoIV/bot_LaLiga/bot_LaLiga.py')
+    with shell_env(TOKEN=os.environ['TOKEN'], USER_BD=os.environ['USER_BD'], PASS_BD=os.environ['PASS_BD']):
+        run ('sudo supervisorctl start La_Liga_bot')
+    #run ('sudo python proyectoIV/bot_LaLiga/bot_LaLiga.py')
 
 def detener():
-    run ('kill -9 $(pidof python)')
+    run ("sudo supervisorctl stop La_Liga_bot")
 
 def borrar():
     run ('rm -rf proyectoIV')
 
 def testear():
-    run ('cd proyectoIV && make test')
+    with shell_env(TOKEN=os.environ['TOKEN'], USER_BD=os.environ['USER_BD'], PASS_BD=os.environ['PASS_BD']):
+        run ('cd IV/ &&  python p_deportivas_bot/test_p_deportivas_bot.py')
 
 def instalar():
-    run ('cd proyectoIV && make install')
+    run ('cd proyectoIV && make bot_LaLiga/install')
+
+def recargar():
+    run("sudo supervisorctl reload")
